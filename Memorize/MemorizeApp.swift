4b007788ceb03510ab4ba21057c9c5dfa73dd0c3
Fredson Silva <92945868+silvafredson@users.noreply.github.com>
2022-03-26 16:29:06 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Fredson Silva on 17/03/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
